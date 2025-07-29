21b8012778a51262c71cb57953d70e76c2385c61
yennhi <yennhii.dyn@gmail.com>
2025-07-29 16:19:35 +0700

    tach js và html và css và thêm route

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json()); 
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/test', (req, res) => {
+  res.sendFile(path.join(__dirname, 'test.html'));
+});
 
 // Add CORS middleware
 app.use((req, res, next) => {
